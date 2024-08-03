69c6cfd399123f3076cddc93dee430d6a557a44e
Omansh Sharma <omansh210305@gmail.com>
2024-08-03 14:19:43 +0530

    Safe Commit

--- server/models/User.js
+++ server/models/User.js
@@ -1,3 +1,20 @@
+const mongoose = require('mongoose');
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect('mongodb://localhost:27017/collab_drawing_platform', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+     
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(`MongoDB Connection Error: ${err.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
+
 const Schema = mongoose.Schema;
 
 
